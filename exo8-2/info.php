7619091b28e067ed81be09f89d6eec62e784b99b
FredericF <frederic.fauquembert@sfr.fr>
2018-08-23 12:05:25 +0200

    ajout condition (if) pour sécurité

--- exo8-2/info.php
+++ exo8-2/info.php
@@ -12,9 +12,9 @@ session_start(); // On démarre la session AVANT tout code HTML
         <h1>Exercice 2  Partie 8</h1>
         <p>Re-bonjour !</p>
         <!-- recupération et affichage avec $_SESSION des infos du visiteur -->
-        <p>
-           Je me souviens de vous ! Vous êtes <?php echo $_SESSION['prenom'] . ' ' . $_SESSION['nom']; ?> !<br />
-            Et vous avez <?php echo $_SESSION['age']; ?> ans.
-        </p>
+        <?php if(!empty($_SESSION['prenom'])) { ?>
+        <p>Je me souviens de vous ! Vous êtes <?php echo $_SESSION['prenom']; } ?>
+        <?php ?><?php if(!empty($_SESSION['nom'])) { echo $_SESSION['nom']; ?> !</p>
+        <p><?php } ?>Et vous avez <?php if(!empty($_SESSION['age'])) { echo $_SESSION['age']; }?> ans.</p>
     </body>
 </html>
