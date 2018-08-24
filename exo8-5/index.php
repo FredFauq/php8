4ac1614c80cab814659dc0b549b731070d8306e8
FredericF <frederic.fauquembert@sfr.fr>
2018-08-24 09:43:36 +0200

    Exercice5 PHP partie 8

--- exo8-5/index.php
+++ exo8-5/index.php
@@ -1,9 +1,14 @@
 <?php
-// isset vérifie que les login et password existent. 
-if(isset($_POST['login']) && isset($_POST['password'])){
-// Définition des cookies
+// vérifie que le login existe et n'est pas vide. 
+if(!empty($_POST['login'])) {
+// Définition du cookie
 setcookie('login', $_POST['login'], time() + 365 * 24 * 3600, '/', null, false, true); //crée le cookie pour le login
-setcookie('password', $_POST['password'], time() + 365 * 24 * 3600,'/', null, false, true);} // créer le cookie pr le password
+}
+// vérifie que le password existe et n'est pas vide. 
+if(!empty($_POST['password'])) {
+// Définition du cookie
+ setcookie('password', $_POST['password'], time() + 365 * 24 * 3600,'/', null, false, true); // créer le cookie pr le password
+}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -15,11 +20,21 @@ setcookie('password', $_POST['password'], time() + 365 * 24 * 3600,'/', null, fa
     <body>
         <h1>Exercice 5 Partie 8</h1>
         <p>Veuillez modifier votre identifiant et mot de passe, svp !</p>
-      <!-- creéation du formulaire -->
+      <!-- création du formulaire -->
     <form method="POST" action="index.php" enctype="multipart/form-data">
         <div><label>Votre login : <input type="text" name="login" id="login"></label></div>
         <div><label>Votre mot de passe : <input type="password" name="password" id="password"></label></div>
         <div><input type="submit" value="Valider"/></div>
+        <p>
+        <?php
+        echo 'Votre login est : ' . $_COOKIE['login']
+        ?>
+        </p>
+        <p>
+        <?php
+        echo 'Votre password est : ' . $_COOKIE['password']       
+        ?>
+        </p>
     </form>
     </body>
 </html>
